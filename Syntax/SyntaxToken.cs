f985a83e0f70913d1aef0178eaeaa2ac6c651519
Bruno Pardini <brunoalan2107@gmail.com>
2026-02-20 12:02:10 -0700

    Eliminacion del analizador semantico

--- Syntax/SyntaxToken.cs
+++ Syntax/SyntaxToken.cs
@@ -7,12 +7,10 @@ using System.Collections.Generic;
 
 namespace Compilador_AII.Syntax
 {
-    // 1. Heredamos de SyntaxNode
-    public class SyntaxToken : SyntaxNode
+    
+    public class SyntaxToken
     {
-        // 2. Hacemos override a la propiedad Kind exigida por SyntaxNode
-        public override SyntaxKind Kind { get; }
-
+        public SyntaxKind Kind { get; }
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
@@ -23,12 +21,6 @@ namespace Compilador_AII.Syntax
             Position = position;
             Text = text;
             Value = value;
-        }
-
-        // 3. Implementamos los hijos (Un token no tiene hijos, devolvemos vacío)
-        public override IEnumerable<SyntaxNode> GetChildren()
-        {
-            return Enumerable.Empty<SyntaxNode>();
         }
     }
 }
